99e1fc85935a41abc1cfeb16a7be308d19ee2492
George Mitsikaris <george.mitsikaris@gmail.com>
2021-12-15 20:13:21 +0200

    Added tests for form layout

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const SignupPage = () => {
-  return (
-    <div>
-      <h1>Sign Up</h1>
-    </div>
-  )
-}
+	return (
+		<div>
+			<h1>Sign Up</h1>
+			<label htmlFor="username">Username</label>
+			<input id="username" />
+			<label htmlFor="email">Email</label>
+			<input id="email" />
+			<label htmlFor="password">Password</label>
+			<input id="password" type="password" />
+			<label htmlFor="passwordRepeat">Repeat Password</label>
+      <input id="passwordRepeat" type="password" />
+      <button disabled>Sign Up</button>
+		</div>
+	);
+};
 
-export default SignupPage
+export default SignupPage;
--- src/pages/SignupPage.spec.js
+++ src/pages/SignupPage.spec.js
@@ -1,8 +1,60 @@
-import { render, screen } from '@testing-library/react';
-import SignupPage from './SignupPage';
+import { render, screen } from "@testing-library/react";
+import SignupPage from "./SignupPage";
 
-it('has header', () => {
-  render(<SignupPage />);
-  const header = screen.queryByRole('heading', { name: 'Sign Up' });
-  expect(header).toBeInTheDocument();
-})
+describe("The signup page", () => {
+	describe("Layout", () => {
+		it("has header", () => {
+			render(<SignupPage />);
+			const header = screen.queryByRole("heading", { name: "Sign Up" });
+			expect(header).toBeInTheDocument();
+    });
+    
+    it('has username input', () => {
+      render(<SignupPage />);
+      const input = screen.getByLabelText('Username');
+      expect(input).toBeInTheDocument();
+    })
+    
+    it("has email input", () => {
+			render(<SignupPage />);
+			const input = screen.getByLabelText("Email");
+			expect(input).toBeInTheDocument();
+		});
+    
+    it("has password input", () => {
+			render(<SignupPage />);
+			const input = screen.getByLabelText("Password");
+			expect(input).toBeInTheDocument();
+		});
+    
+    it("has password type for password input", () => {
+			render(<SignupPage />);
+			const input = screen.getByLabelText("Password");
+			expect(input.type).toBe('password');
+		});
+    
+    it("has password repeat input", () => {
+			render(<SignupPage />);
+			const input = screen.getByLabelText("Repeat Password");
+			expect(input).toBeInTheDocument();
+		});
+
+		it("has password type for password repeat input", () => {
+			render(<SignupPage />);
+			const input = screen.getByLabelText("Repeat Password");
+			expect(input.type).toBe("password");
+    });
+    
+    it('has sign up button', () => {
+      render(<SignupPage />);
+      const signUpButton = screen.queryByRole('button', { name: 'Sign Up' });
+      expect(signUpButton).toBeInTheDocument();
+    })
+    
+    it("disables sign up button initially", () => {
+			render(<SignupPage />);
+			const signUpButton = screen.queryByRole("button", { name: "Sign Up" });
+			expect(signUpButton).toBeDisabled();
+		});
+	});
+});
