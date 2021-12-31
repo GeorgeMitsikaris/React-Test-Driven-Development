bc0236a7d4ed506d733b8e1313607b3c10870717
George Mitsikaris <george.mitsikaris@gmail.com>
2021-12-31 21:58:36 +0200

    Added tests for page navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import LanguageSelector from './components/LanguageSelector';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
+import LanguageSelector from "./components/LanguageSelector";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 import UserPage from "./pages/UserPage";
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
 
 function App() {
-  return (
+	const { t } = useTranslation();
+	const [path, setPath] = useState(window.location.pathname);
+	const onClickLink = (e) => {
+		e.preventDefault();
+		const path = e.target.attributes.href.value;
+		window.history.pushState({}, "", path);
+		setPath(path);
+	};
+	return (
 		<div className="container">
-			{window.location.pathname === "/" && <HomePage />}
-			{window.location.pathname === "/signup" && <SignupPage />}
-			{window.location.pathname === "/login" && <LoginPage />}
-			{window.location.pathname.startsWith("/user") && <UserPage />}
+			<a href="/" title="Home" onClick={onClickLink}>
+				Hoaxify
+			</a>
+			<a href="/signup" title="Sign Up" onClick={onClickLink}>
+				{t("signUp")}
+			</a>
+			<a href="/login" title="Login" onClick={onClickLink}>
+				Login
+			</a>
+			{path === "/" && <HomePage />}
+			{path === "/signup" && <SignupPage />}
+			{path === "/login" && <LoginPage />}
+			{path.startsWith("/user") && <UserPage />}
 			<LanguageSelector />
 		</div>
 	);
--- src/App.spec.js
+++ src/App.spec.js
@@ -1,11 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
 describe("Routing", () => {
-  const setup = (path) => {
-    window.history.pushState({}, '', path);
-    render(<App />);
-  }
+	const setup = (path) => {
+		window.history.pushState({}, "", path);
+		render(<App />);
+	};
 
 	it.each`
 		path         | pageTestId
@@ -15,11 +16,11 @@ describe("Routing", () => {
 		${"/user/1"} | ${"user-page"}
 		${"/user/2"} | ${"user-page"}
 	`("displays $pageTestId when path is $path", ({ path, pageTestId }) => {
-		setup(path, pageTestId)
+		setup(path, pageTestId);
 		const page = screen.queryByTestId(pageTestId);
 		expect(page).toBeInTheDocument();
 	});
-  
+
 	it.each`
 		path         | pageTestId
 		${"/"}       | ${"signup-page"}
@@ -37,10 +38,35 @@ describe("Routing", () => {
 	`(
 		"does not display $pageTestId when path is $path",
 		({ path, pageTestId }) => {
-      setup(path);
+			setup(path);
 			const page = screen.queryByTestId(pageTestId);
 			expect(page).not.toBeInTheDocument();
 		}
-	);  
+	);
 
+	it.each`
+		targetPage
+		${"Home"}
+		${"Sign Up"}
+		${"Login"}
+	`("has link to homepage on NavBar", ({ targetPage }) => {
+		setup("/");
+		const link = screen.getByRole("link", { name: targetPage });
+		expect(link).toBeInTheDocument();
+	});
+
+	it.each` 
+		initialPath  | clickinTo    | visiblePage
+		${"/"}       | ${"Sign Up"} | ${"signup-page"}
+		${"/signup"} | ${"Home"}    | ${"home-page"}
+		${"/signup"} | ${"Login"}    | ${"login-page"}
+	`(
+		"displays sign up page after clicking sign up link",
+		({ initialPath, clickinTo, visiblePage }) => {
+			setup(initialPath);
+			const link = screen.getByRole("link", { name: clickinTo });
+			userEvent.click(link);
+			expect(screen.getByTestId(visiblePage)).toBeInTheDocument();
+		}
+	);
 });
