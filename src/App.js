8bc37d21e790c54eac6ab3f4c6f567d417f4c49e
George Mitsikaris <george.mitsikaris@gmail.com>
2022-01-02 17:07:20 +0200

    Created tests for routing on activation page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import UserPage from "./pages/UserPage";
 import { useTranslation } from "react-i18next";
 import logo from './assets/hoaxify.png';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import AccountActivationPage from "./pages/AccountActivationPage";
 
 function App() {
 	const { t } = useTranslation();
@@ -14,27 +15,15 @@ function App() {
 		<BrowserRouter>
 			<nav className="navbar navbar-expand navbar-light bg-light shadow-sm">
 				<div className="container">
-					<Link
-						className="navbar-brand"
-						to="/"
-						title="Home"
-					>
+					<Link className="navbar-brand" to="/" title="Home">
 						<img src={logo} alt="Hoaxify Logo" width="60" />
 						Hoaxify
 					</Link>
 					<ul className="navbar-nav">
-						<Link
-							className="nav-link"
-							to="/signup"
-							title="Sign Up"
-						>
+						<Link className="nav-link" to="/signup" title="Sign Up">
 							{t("signUp")}
 						</Link>
-						<Link
-							className="nav-link"
-							to="/login"
-							title="Login"
-						>
+						<Link className="nav-link" to="/login" title="Login">
 							Login
 						</Link>
 					</ul>
@@ -44,7 +33,8 @@ function App() {
 				<Route path="/" exact component={HomePage} />
 				<Route path="/signup" component={SignupPage} />
 				<Route path="/login" component={LoginPage} />
-				<Route path="/user/:id" component={UserPage} />			
+				<Route path="/user/:id" component={UserPage} />
+				<Route path="/activate/:id" component={AccountActivationPage} /> 
 				<LanguageSelector />
 			</div>
 		</BrowserRouter>
